d82bbb319ef31bd53f29116ce9f5c375e3113c17
Nataliia <natalabojko@gmail.com>
2024-03-27 12:30:47 +0200

    qwe

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,8 +1,25 @@
+import Image from "next/image";
 import React from "react";
 import styles from "./Hero.module.scss";
+import microphone from "../../../public/images/microphone.png";
 
 const Hero = () => {
-  return <section id={styles["hero"]}>Hero</section>;
+  return (
+    <section id={styles["hero"]}>
+      <div className={`container ${styles.container}`}>
+        <div className={styles.nameWrapper}>
+          <h1>
+            Яна <br />
+            Полонська
+          </h1>
+          <p>Викладач вокалу</p>
+        </div>
+        <div className={styles.imgWrapper}>
+          <Image src={microphone} alt="Picture of the author" fill />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
