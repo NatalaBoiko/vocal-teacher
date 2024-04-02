e345baac3ab2c738c98d23c9b2eb3d30dae26a33
Nataliia <natalabojko@gmail.com>
2024-04-02 16:38:57 +0300

    about

--- src/components/AboutSlider/AboutSlider.jsx
+++ src/components/AboutSlider/AboutSlider.jsx
@@ -1,12 +1,10 @@
 "use client";
 
 import React from "react";
+import Image from "next/image";
+import { aboutImages } from "@/data/aboutImages";
+
 import { Swiper, SwiperSlide } from "swiper/react";
-import "./AboutSlider.css";
-
-import "swiper/css";
-import "swiper/css/pagination";
-
 import {
   Navigation,
   Pagination,
@@ -15,17 +13,33 @@ import {
   EffectCoverflow,
   Autoplay,
 } from "swiper/modules";
-import Image from "next/image";
+import "./AboutSlider.css";
+import "swiper/css";
+import "swiper/css/pagination";
 
 const AboutSlider = () => {
+  const slide = aboutImages.map((el, i) => {
+    return (
+      <SwiperSlide key={i}>
+        <Image
+          src={el.src}
+          fill
+          alt={el.title}
+          sizes="(max-width: 768px) 100vw, 50vw"
+          title={el.title}
+        />
+      </SwiperSlide>
+    );
+  });
+
   return (
     <Swiper
-      // cssMode={true}
+      //   cssMode={true}
       loop={true}
       pagination={{
         clickable: true,
       }}
-      // mousewheel={true}
+      mousewheel={true}
       keyboard={true}
       effect={"coverflow"}
       grabCursor={true}
@@ -40,11 +54,12 @@ const AboutSlider = () => {
       }}
       spaceBetween={30}
       speed={2000}
-      //   autoplay={{
-      //     delay: 2000,
-      //     disableOnInteraction: false,
-      //     pauseOnMouseEnter: true,
-      //   }}
+      autoplay={{
+        delay: 2000,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true,
+      }}
+      pauseOnMouseEnter={true}
       modules={[
         Navigation,
         Pagination,
@@ -55,12 +70,7 @@ const AboutSlider = () => {
       ]}
       className="aboutSlider"
     >
-      <SwiperSlide>
-        <Image src="/images/Yana_1.jpeg" fill />
-      </SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
+      {slide}
     </Swiper>
   );
 };
---
+++ src/data/aboutImages.js
@@ -0,0 +1,18 @@
+export const aboutImages = [
+  {
+    src: "/images/Yana_1.webp",
+    title: "In love with juzz",
+  },
+  {
+    src: "/images/Yana_2.webp",
+    title: "In love with juzz",
+  },
+  {
+    src: "/images/Yana_3.webp",
+    title: "In love with juzz",
+  },
+  {
+    src: "/images/Yana_4.webp",
+    title: "In love with juzz",
+  },
+];
